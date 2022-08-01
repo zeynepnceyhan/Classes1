9fbf343278ad47e59cbd921033d409c1cd063fd5
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-01 14:13:56 +0300

    Classes (OOP)
    internship

---
+++ Classes1/Classes1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Console.ReadLine();
+        }
+        class CustomerManager
+        {
+            public void Add()
+            {
+                Console.WriteLine("Customer Added!");
+            }
+            public void Update()
+            {
+                Console.WriteLine("Customer Updated!");
+            }
+        }
+
+        class ProductManager
+        {
+            public void Add()
+            {
+                Console.WriteLine("Product Added!");
+            }
+            public void Update()
+            {
+                Console.WriteLine("Product Updated!");
+            }
+        }
+    }
+}
